1bea4c70cad66674d18b5e671dc06c9a03af4126
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-10-24 00:46:37 +0900

    #1 styleを整え中

--- src/pages/apis/[id].tsx
+++ src/pages/apis/[id].tsx
@@ -1,8 +1,11 @@
-import { Input } from 'antd';
+import { EditFilled } from '@ant-design/icons';
+import { Button, Input, Tooltip } from 'antd';
 import { NextPage } from 'next';
 import { Api } from 'src/types/api';
 
 import { apiRepository } from '../../repository/apis';
+
+const { TextArea } = Input;
 
 interface Props {
   api: Api;
@@ -12,10 +15,23 @@ const ApiPage: NextPage<Props> = ({ api }) => {
   return (
     <div>
       <h1>{api?.name}</h1>
-      <h2>{api?.url}</h2>
-      <Input placeholder="url" value={api?.url} />
+      <h2>URL</h2>
+      <Input placeholder="url" value={api?.url} style={{ marginBottom: 20 }} />
       <h2>Description</h2>
-      <Input placeholder="description" value={api?.description} />
+      <TextArea placeholder="description" rows={4} value={api?.description} style={{ marginBottom: 20 }} />
+      <h2>Model</h2>
+      {/* コンポーネント化する */}
+      <span style={{ fontSize: 16 }}>model name</span>
+      <Tooltip title="Edit model">
+        <Button style={{ marginLeft: 10, marginBottom: 20 }} type="primary" shape="circle" icon={<EditFilled />} />
+      </Tooltip>
+      <h2>
+        Methods
+        <Button style={{ marginLeft: 10 }} type="primary">
+          + New
+        </Button>
+      </h2>
+      <div>メソッド一覧が入る予定。</div>
     </div>
   );
 };
